39936e779ec7262495120275e714e38ed4c284f6
zspringer <zspringer.springer@gmail.com>
2017-04-18 06:59:23 -0600

    Add files via upload

---
+++ battleship.js
@@ -0,0 +1,16 @@
+/* This is just a start */
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a value between 0-6");
+    }else{
+        guesses = guesses + 1;
+    }
+}
